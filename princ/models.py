aaac87773020f9b708586be6d6cb4f197413546b
japhet <sagoua1@gmail.com>
2020-03-04 16:33:22 +0100

    gestion des medias

--- princ/models.py
+++ princ/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -12,10 +13,30 @@ class Job (models.Model):
         ("Freelance","Freelance")
     )
 
+    Categories = (
+        ("enseignement","Enseignement education"),
+        ("earketing","Marketing"),
+        ("eelemarketing","Telemarketing"),
+        ("software","Developpement d'application & web"),
+        ("batiment","Batiments et design"),
+        ("administration","Administration"),
+        ("autre","Autres")
+    )
+
+    Experiences = (
+        ("non","Non spécifié"),
+        ("Debutant","Debutant"),
+        ("junior","1 à 2 ans d'experiences"),
+        ("moyen", "2 à 6 ans d'experiences"),
+        ("senior","plus de 6 ans d'experiences")
+    )
+
     posteur = models.ForeignKey('Compte', on_delete= models.CASCADE, blank=True)
     titre = models.CharField(max_length=200, blank=False)
+    categorie = models.CharField(max_length=200, blank=True, default="autre", choices=Categories)
     lieu = models.CharField(max_length=200)
     type = models.CharField(max_length=100, choices=jobtype)
+    experience = models.CharField(max_length=100, choices=Experiences, blank=True, default="None")
     pub_date = models.DateTimeField(auto_now=True)
     description = models.TextField()
     salaire_max = models.IntegerField(null=True)
@@ -24,13 +45,20 @@ class Job (models.Model):
     def __str__(self):
         return self.titre
     
+    
+    
 
 class Compte(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     description = models.TextField()
+    img_profil = models.FileField(upload_to="img/candiateds", default = "img/svg_icon/1.svg")
     register_date = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.user.username
+
+class Candidat(models.Model):
+    compte = models.OneToOneField(Compte, on_delete=models.CASCADE)
+    qualifications = models.CharField(max_length=100)
     
     
--- princ/views.py
+++ princ/views.py
@@ -8,7 +8,8 @@ from .models import *
 
 def home(request):
     jobs = Job.objects.order_by("-pub_date")[:5]
-    return render(request,"home.html",context={"jobs":jobs})
+    cat_list = Job.Categories
+    return render(request,"home.html",context={"jobs":jobs,"cat_list":cat_list})
 
 class JobListView(ListView):
     model = Job
@@ -20,6 +21,7 @@ class JobListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["nb_jobs"] = Job.objects.count()
+        context["cat_list"] = Job.Categories
         return context
 
 
